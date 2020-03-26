495552c284f111081a28a15ec251ec5b7783ec91
James Hay <jameshay218@gmail.com>
2020-03-26 16:20:42 +0000

    Corrected likelihood

--- code.R
+++ code.R
@@ -6,15 +6,15 @@ library(doParallel)
 library(patchwork)
 setwd("~/Documents/GitHub/gupta_model_check")
 
-rerun_fits <- FALSE
+rerun_fits <- TRUE
 
 n_clusters <- 9
 cl <- makeCluster(n_clusters)
 registerDoParallel(cl)
 
-mcmcPars1 <- c("iterations"=150000,"popt"=0.44,"opt_freq"=1000,
+mcmcPars1 <- c("iterations"=100000,"popt"=0.44,"opt_freq"=1000,
               "thin"=10,"adaptive_period"=50000,"save_block"=1000)
-mcmcPars2 <- c("iterations"=400000,"popt"=0.234,"opt_freq"=1000,
+mcmcPars2 <- c("iterations"=200000,"popt"=0.234,"opt_freq"=1000,
                "thin"=10,"adaptive_period"=100000,"save_block"=1000)
 
 get_index_par <- function(chain, index){
@@ -54,21 +54,20 @@ SIR_odes <- function(t, x, params) {
   dZ <- beta*y*(1-z)
   
   ## Note need to shift this number forward by psi days post-hoc
-  dDead <- N*rho*theta*z
-  list(c(dY,dZ,dDead))
+  list(c(dY,dZ))
 }
 
 ## ROUGH PARAMETERS FROM THE PAPER
-sigma <- 1/3.25 ## 1/Infectious period
-R0 <- 2.25 ## Basic reproductive number
-rho <- 0.001 ## Proportion of population at risk of severe disease
+sigma <- 1/4.5 ## 1/Infectious period
+R0 <- 2.5 ## Basic reproductive number
+rho <- 0.01 ## Proportion of population at risk of severe disease
 theta <- 0.14 ## Probability of dying with severe disease
 psi <- 17 ## Time between infection and death
-t0 <- 0
-psi <- 15.5
+t0 <- 31 + 28
+psi <- 17
 
 ## Population of UK
-N <- 66800000
+N <- 66870000
 
 ## EXTRACT AND CLEAN DATA
 ## Number dead in UK to date
@@ -76,7 +75,7 @@ obs_deaths <- read_csv("~/Documents/GitHub/COVID-19/csse_covid_19_data/csse_covi
 obs_deaths <- obs_deaths %>% filter(`Country/Region` == "United Kingdom") %>% select(-c("Province/State","Country/Region","Lat","Long"))
 obs_deaths <- colSums(obs_deaths)
 obs_deaths <- data.frame(time=names(obs_deaths),dead=obs_deaths)
-obs_deaths$time <- as.Date(as.character(obs_deaths$time),origin="2020-01-01", format="%m/%d/%y")
+obs_deaths$time <- as.Date(as.character(obs_deaths$time),origin="2019-12-01", format="%m/%d/%y")
 obs_deaths <- obs_deaths %>% filter(time <= "2020-03-19")
 obs_deaths <- obs_deaths %>% filter(time >= "2020-03-05")
 data <- obs_deaths
@@ -86,50 +85,48 @@ data <- obs_deaths
 t <- seq(0,100,by=1)
 
 ## Note starting conditions for population size - we're working per capita here
-results <- as.data.frame(deSolve::ode(y=c(y=1/N,z=1/N,Dead=0),
+y0 <- 1/N
+results <- as.data.frame(deSolve::ode(y=c(y=y0,z=y0),
                                       times=t, func=SIR_odes,
                                       parms=c(R0,sigma, rho, theta, N)))
 
+
 ## Deaths for that time are actually reported psi days later
-results$time_deaths <- results$time + psi
+results$time_deaths <- results$time + psi + t0
+results$time <- results$time + t0
 results$susc <- 1 - results$z
-results$time <- as.Date(results$time, origin="2020-01-27")
+results$dead <- N*rho*theta*results$z
+results$time <- as.Date(results$time, origin="2019-12-01")
+results$time_deaths <- as.Date(results$time_deaths, origin="2019-12-01")
+
+par(mfrow=c(2,1))
 plot(results[,c("time","susc")], type='l',col="green",xlab="Time (days)", ylab="Proportion of population",ylim=c(0,1))
 lines(results[,c("time","z")],col="blue")
 lines(results[,c("time","y")],col="red")
-abline(v=as.Date(results[results$Dead > 1,][1,"time_deaths"],origin="2020-01-27"),lty=2)
 legend("topright", title="Key",
        legend=c("Proportion infectious","Proportion no longer susceptible","Proportion susceptible"),
        col=c("red","blue","green"),
        lty=c(1,1,1))
+plot(results[,c("time_deaths","dead")],type='l')
 
 
 ##################################
 ## NOW SET UP CODE TO FIT MODEL
-
 t_long <- seq(0,365,by=1)
 pars <- c(R0, sigma, rho, theta, N, psi, t0)
 names(pars) <- c("R0","sigma","rho","theta","N","psi","t0")
 ## Putting model solving code in a function for later use
 solve_model <- function(pars, t){
   N <- pars["N"]
-  psi <- pars["psi"]
-  t0 <- pars["t0"]
   ## Note starting conditions for population size - we're working per capita here
-  results <- as.data.frame(deSolve::ode(y=c(y=1/N,z=1/N,Dead=0),
+  results <- as.data.frame(deSolve::ode(y=c(y=1/N,z=1/N),
                                         times=t, func=SIR_odes,
                                         parms=pars))
 
-  ## For solve deaths, shift forward in time as will only be observed
-  ## t0 + psi days after t=0
-  predicted <- results %>% 
-    mutate(time = as.integer(time + t0 + psi)) %>%
-    mutate(time = as.Date(time, origin="2020-01-01"))
-  
-  return(predicted)
+  return(results)
 }
 
-## We need to put our likleihood function in a closure environment
+## We need to put our likelihood function in a closure environment
 ## It's important to write the function in this form!
 create_lik <- function(parTab, data, PRIOR_FUNC,t, PRIOR_ONLY=FALSE){
   par_names <- parTab$names
@@ -143,11 +140,24 @@ create_lik <- function(parTab, data, PRIOR_FUNC,t, PRIOR_ONLY=FALSE){
     names(pars) <- par_names
     ## Solve model
     if(!PRIOR_ONLY){
-      predicted <- solve_model(pars, t)
+      results <- solve_model(pars, t)
+      
+      ## Deaths for that time are actually reported psi days later
+      psi <- pars["psi"]
+      t0 <- pars["t0"]
+      theta <- pars["theta"]
+      rho <- pars["rho"]
+      
+      ## Shift times and convert to dates
+      results$time_deaths <- results$time + as.integer(psi + t0)
+      results$time <- results$time + as.integer(t0)
+      results$dead <- N*rho*theta*results$z
+      results$time <- as.Date(results$time, origin="2019-12-01")
+      results$time_deaths <- as.Date(results$time_deaths, origin="2019-12-01")
       
       ## Get deaths that match data
-      predicted1 <-  predicted %>%
-        filter(time <= max_time & time >= min_time) %>% pull(Dead)
+      predicted1 <-  results %>%
+        filter(time_deaths <= max_time & time_deaths >= min_time) %>% pull(dead)
       lik <- sum(dpois(x=obs_dead,lambda=predicted1,log=TRUE))
     } else {
       lik <- 0
@@ -163,12 +173,13 @@ parTab <- data.frame(names=c("R0","sigma", "rho", "theta", "N", "psi", "t0"),
                      fixed=c(0,0,0,0,1,0,0),
                      steps=c(0.1,0.1,0.1,0.1,0.1,0.1,0.1),
                      lower_bound=c(1,0,0,0,0,0,0),
-                     upper_bound=c(10,10,0.1,10,100000000,25,45))
+                     upper_bound=c(10,10,0.1,10,100000000,25,75))
 
 
 ## Test the model solves
 f <- create_lik(parTab, data, NULL, t=t_long)
 f(pars)
+
 ## Starting points, chosen pseudo at random
 ## seeding chains for SIR models is hard with deSolve,
 ## so I've chosen points near the true values.
@@ -205,9 +216,10 @@ prior_func_me <- function(pars){
 top_wd <- getwd()
 
 if(rerun_fits){
-  res <- foreach(i=seq_along(runnames),.packages=c("lazymcmc","tidyverse")) %dopar% {
+  
+res <- foreach(i=seq_along(runnames),.packages=c("lazymcmc","tidyverse")) %dopar% {
     setwd(top_wd)
-    setwd("chains2")
+    setwd("chains")
     
     if(!file.exists(runnames[i])) {
       dir.create(runnames[i])
@@ -248,12 +260,12 @@ if(rerun_fits){
 
 ## Read in the MCMC chains
 setwd(top_wd)
-setwd("chains2")
+setwd("chains")
 
 all_runs <- list.files()
 all_chains <- NULL
 for(run in all_runs){
-  chains <- load_mcmc_chains(paste0(top_wd, "/chains2/",run),parTab,TRUE,1,mcmcPars2["adaptive_period"])
+  chains <- load_mcmc_chains(paste0(top_wd, "/chains/",run),parTab,TRUE,1,200000)
   
   tmp_chain <- as.data.frame(chains[[2]])
   tmp_chain$sampno <- 1:nrow(tmp_chain)
@@ -261,9 +273,9 @@ for(run in all_runs){
   tmp_chain <- reshape2::melt(tmp_chain, id.vars=c("sampno","run"))
   all_chains <- rbind(all_chains, tmp_chain)
   
-  #pdf(paste0(top_wd,"/",run,"_chains.pdf"))
-  #plot(chains[[1]])
-  #dev.off()
+  pdf(paste0(top_wd,"/",run,"_chains.pdf"))
+  plot(chains[[1]])
+  dev.off()
 }
 all_chains1 <- all_chains
 all_chains <- all_chains %>% mutate(value=ifelse(variable=="sigma", 1/value, value))
@@ -322,17 +334,17 @@ p3 <- ggplot(all_chains[all_chains$group == "new",]) +
   theme_bw() +
   theme(legend.position=c(0.7,0.2))
 
-#png("densities_original_low.png",height=5,width=8,res=300,units="in")
-#p1
-#dev.off()
+png(paste0(top_wd,"/plots/densities_original_low.png"),height=5,width=8,res=300,units="in")
+p1
+dev.off()
 
-#png("densities_original_high.png",height=5,width=8,res=300,units="in")
-#p2
-#dev.off()
+png(paste0(top_wd,"/plots/densities_original_high.png"),height=5,width=8,res=300,units="in")
+p2
+dev.off()
 
-#png("densities_new.png",height=5,width=8,res=300,units="in")
-#p3
-#dev.off()
+png(paste0(top_wd,"/plots/densities_new.png"),height=5,width=8,res=300,units="in")
+p3
+dev.off()
 
 all_chains_subset <- all_chains %>% filter(Version %in% c("After fitting to data, original", "After fitting to data, my analysis") & variable %in% var_key[names(var_key) %in% c("R0","sigma","rho","t0")])
 all_chains_subset$variable <- as.character(all_chains_subset$variable)
@@ -341,65 +353,61 @@ all_chains_subset <- all_chains_subset %>% pivot_wider(names_from=variable,value
 ## Plot the 2D densities
 p4 <- ggplot(all_chains_subset) + 
   geom_hex(aes(x=`sigma`,y=`R[0]`,fill=..density..),bins=100) +
-  ylab("Basic reproductive number") + xlab("Infectious period in days") +
+  ylab("Basic reproductive number, R[0]") + xlab("Infectious period in days") +
   facet_wrap(~Version) +
   scale_fill_gradient2(low="#5E4FA2",mid="#FAFDB8",high="#9E0142",midpoint= 0.008) +
   theme_bw()
 
 p5 <- ggplot(all_chains_subset) + 
-  geom_hex(aes(y=`rho`,x=as.Date(`Seed_date`,origin="2020-01-01"),fill=..density..),bins=100) +
-  xlab("Seed date") + ylab("Proportion of population at\n risk of severe disease") +
+  geom_hex(aes(y=`rho`,x=as.Date(`Seed_date`,origin="2019-12-01"),fill=..density..),bins=100) +
+  xlab("Seed date") + ylab("Proportion of population at\n risk of severe disease, ρ") +
   scale_fill_gradient2(low="#5E4FA2",mid="#FAFDB8",high="#9E0142",midpoint= 0.008) +
   facet_wrap(~Version) + theme_bw()
 
-#png("correlations.png",height=6,width=8,res=300,units="in")
-#p4/p5
-#dev.off()
+png(paste0(top_wd,"/plots/correlations.png"),height=6,width=8,res=300,units="in")
+p4/p5
+dev.off()
 
 
 ## Calculate 95% CI on proportion infected
 setwd(top_wd)
-setwd("chains2")
+setwd("chains")
 run <- "fitting_me/"
-chains <- load_mcmc_chains(paste0(top_wd, "/chains2/",run),parTab,FALSE,1,mcmcPars2["adaptive_period"])
+chains <- load_mcmc_chains(paste0(top_wd, "/chains/",run),parTab,FALSE,1,200000)
 chain <- as.data.frame(chains[[2]])  
 chain$sampno <- 1:nrow(chain)
-samps <- sample(unique(chain$sampno), 10000)
+samps <- sample(unique(chain$sampno), 30000)
 
-solve_model2 <- function(pars, t){
-  N <- pars["N"]
-  psi <- pars["psi"]
-  t0 <- pars["t0"]
-  ## Note starting conditions for population size - we're working per capita here
-  results <- as.data.frame(deSolve::ode(y=c(y=1/N,z=1/N,Dead=0),
-                                        times=t, func=SIR_odes,
-                                        parms=pars))
-  
-  ## To solve for real time, shift forward by t0 days
-  predicted <- results %>% 
-    mutate(time = as.integer(time + t0)) %>%
-    mutate(time = as.Date(time, origin="2020-01-01"))
-  
-  return(predicted)
-}
-
-prop_infected <- numeric(10000)
-rhos <- numeric(10000)
+prop_infected <- numeric(30000)
+rhos <- numeric(30000)
+t0s <- numeric(30000)
 for(i in seq_along(samps)){
   pars <- get_index_par(chain, samps[i])
-  res <- solve_model2(pars, t_long)
+  results <- solve_model(pars, t_long)
   rhos[i] <- pars["rho"]
-  prop_infected[i] <- res[res$time == "2020-03-19","z.N"]
+  t0s[i] <- pars["t0"]
+  psi <- pars["psi"]
+  t0 <- pars["t0"]
+  theta <- pars["theta"]
+  rho <- pars["rho"]
+  results$time_deaths <- results$time + as.integer(psi + t0)
+  results$time <- results$time + as.integer(t0)
+  results$dead <- N*rho*theta*results$z
+  results$time <- as.Date(results$time, origin="2019-12-01")
+  results$time_deaths <- as.Date(results$time_deaths, origin="2019-12-01")
+  
+  prop_infected[i] <- results[results$time == "2020-03-19","z.N"]
 }
-print(paste0("Proportion immune: ", quantile(prop_infected,c(0.025,0.5, 0.975))*100))
+print(paste0("Proportion immune: ", quantile(prop_infected,c(0.01,0.025,0.5, 0.975,0.99))*100))
 
 final_dat <- data.frame(rho=rhos,prop_infected=prop_infected)
 
 p7 <- ggplot(final_dat) + 
   geom_hex(aes(y=`rho`,x=prop_infected,fill=..density..),bins=100) +
-  xlab("Proportion of UK population immune by 19/03/2020") + ylab("Proportion of population at\n risk of severe disease") +
-  scale_fill_gradient2(low="#5E4FA2",mid="#FAFDB8",high="#9E0142",midpoint= 0.0015) + theme_bw() + theme(legend.position=c(0.8,0.7))
-png("final_new.png",height=4,width=5,res=300,units="in")
+  xlab("Proportion of UK population immune by 19/03/2020") + ylab("Proportion of population at\n risk of severe disease ρ") +
+  scale_fill_gradient2(low="#5E4FA2",mid="#FAFDB8",high="#9E0142",midpoint= 0.0008) + theme_bw() + theme(legend.position=c(0.8,0.7))
+
+png(paste0(top_wd,"/plots/final_new.png"),height=4,width=5,res=300,units="in")
 p7
 dev.off()
 
